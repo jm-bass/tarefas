709a0e5e5d27d37b3fc9cb2a49b2f76ca7ddabd1
JM-Bass <joaomariapereire@gmail.com>
2024-08-23 08:10:53 -0300

    Commit 2

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class terefa(models.Model):
+    nome = models.CharField(max_length = 100)
+    satus = models.CharField(max_length = 20)
+    prazo = models.DateField()
